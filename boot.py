8666069c71acd95eb993a3f5030986ec469ad86b
sam blenny <68084116+samblenny@users.noreply.github.com>
2024-07-07 17:05:05 +0000

    prepare for another test

--- boot.py
+++
@@ -1,5 +0,0 @@
-# You can use boot.py to customize your CircuitPython board's USB devices.
-# You can learn about the possibilities at:
-# - https://learn.adafruit.com/customizing-usb-devices-in-circuitpython
-# - https://learn.adafruit.com/welcome-to-circuitpython
-# - https://learn.adafruit.com/circuitpython-essentials/circuitpython-storage
